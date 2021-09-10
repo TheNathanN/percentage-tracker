241bbb2a3c12ca1bb34edceddd7a831bdd58470e
Nathan Nicholson <85798301+TheNathanN@users.noreply.github.com>
2021-09-10 17:04:58 -0500

    Add PercCalc functionality

--- src/components/calc-comps/PercCalc.js
+++ src/components/calc-comps/PercCalc.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './PercCalc.scss';
 
@@ -7,17 +7,33 @@ const PercCalc = () => {
   const [amount, setAmount] = useState();
   const [total, setTotal] = useState();
 
+  useEffect(() => {
+    const percentHandler = () => {
+      const answer = amount / total;
+      const formatAnswer = answer * 100;
+      setPercent(formatAnswer);
+    };
+    percentHandler();
+  }, [amount, total]);
+
   return (
     <div className='perc-container'>
-      $ <input className='dollar-amount' type='number' placeholder='Amount' />
+      ${' '}
+      <input
+        className='dollar-amount'
+        type='number'
+        placeholder='Amount'
+        onChange={e => setAmount(e.target.value)}
+      />
       <span> of </span>${' '}
       <input
         className='dollar-amount'
         type='number'
-        placeholder='Total Portfolio'
+        placeholder='Total Portfolio + Amount'
+        onChange={e => setTotal(e.target.value)}
       />
       <span> = </span>
-      {!percent ? '0%' : `${percent}%`}
+      {!percent || !total ? '0%' : `${Math.round(percent)}%`}
     </div>
   );
 };
