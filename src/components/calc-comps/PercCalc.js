ad5059e35154e8c1d879c22434aaa6605342a0f4
Nathan Nicholson <85798301+TheNathanN@users.noreply.github.com>
2021-09-10 16:45:19 -0500

    Add TotalCalc functionality

--- src/components/calc-comps/PercCalc.js
+++ src/components/calc-comps/PercCalc.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import './PercCalc.scss';
 
 const PercCalc = () => {
+  const [percent, setPercent] = useState();
+  const [amount, setAmount] = useState();
+  const [total, setTotal] = useState();
+
   return (
-    <div>
-      <h2>PercCalc</h2>
+    <div className='perc-container'>
+      $ <input className='dollar-amount' type='number' placeholder='Amount' />
+      <span> of </span>${' '}
+      <input
+        className='dollar-amount'
+        type='number'
+        placeholder='Total Portfolio'
+      />
+      <span> = </span>
+      {!percent ? '0%' : `${percent}%`}
     </div>
   );
 };
--- src/components/calc-comps/TotalCalc.js
+++ src/components/calc-comps/TotalCalc.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './TotalCalc.scss';
 
@@ -7,20 +7,34 @@ const TotalCalc = () => {
   const [amount, setAmount] = useState();
   const [total, setTotal] = useState();
 
+  useEffect(() => {
+    const totalHandler = () => {
+      const formatPerc = Number(percent) / 100;
+      const answer = Number(amount) * formatPerc;
+      setTotal(answer);
+    };
+    totalHandler();
+  }, [percent, amount]);
+
   return (
-    <div className='total-calc'>
-      <div className='total-container'>
-        <input className='total-percent' type='number' placeholder='0' /> % of
-        <span>
-          ${' '}
-          <input
-            className='total-money'
-            type='number'
-            placeholder='Total Portfolio'
-          />
-        </span>
-        = <span>{!total ? '$0.00' : total}</span>
-      </div>
+    <div className='total-container'>
+      <input
+        className='total-percent'
+        type='number'
+        placeholder='0'
+        onChange={e => setPercent(e.target.value)}
+      />{' '}
+      % of
+      <span>
+        ${' '}
+        <input
+          className='total-money'
+          type='number'
+          placeholder='Total Portfolio'
+          onChange={e => setAmount(e.target.value)}
+        />
+      </span>
+      = <span>{!total ? '$0.00' : `$${total.toFixed(2)}`}</span>
     </div>
   );
 };
