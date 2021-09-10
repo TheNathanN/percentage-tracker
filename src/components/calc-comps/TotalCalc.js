8e2b585099431cf550e5c284a7c58b52c74da7db
Nathan Nicholson <85798301+TheNathanN@users.noreply.github.com>
2021-09-10 08:44:20 -0500

    Add and style TotalCalc

--- src/components/calc-comps/TotalCalc.js
+++ src/components/calc-comps/TotalCalc.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './TotalCalc.scss';
 
 const TotalCalc = () => {
+  const [percent, setPercent] = useState();
+  const [amount, setAmount] = useState();
+  const [total, setTotal] = useState();
+
   return (
     <div className='total-calc'>
       <div className='total-container'>
-        <h1>TotalCalc</h1>
+        <input className='total-percent' type='number' placeholder='0' /> % of
+        <span>
+          ${' '}
+          <input
+            className='total-money'
+            type='number'
+            placeholder='Total Portfolio'
+          />
+        </span>
+        = <span>{!total ? '$0.00' : total}</span>
       </div>
     </div>
   );
